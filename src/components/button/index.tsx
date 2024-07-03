adde232d57deabcc59eebd350f4d343f935b2d69
hyunho4532 <kyounguk192@gmail.com>
2024-07-03 22:44:50 +0900

    Dev . letter write feature is currently being implemented. (Component Division, Button Component Define)

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,24 @@
 import { css } from "@emotion/css";
+import React from "react";
 
-export function Button({children}: any) {
+
+export function Buttons({children}: { children: React.ReactNode }) {   
+    return (
+        <div>{children}</div>
+    )
+}
+
+export function Button({children}: { children: React.ReactNode }) {
     return (
         <button className={css`
             width: 180px;
             height: 120px;
-            margin-left: 12px;
+            margin-left: 8px;
+            transition: transform 0.7s;
+
+            &:hover {
+                transform: scale(1.10)
+            }
         `}>{children}</button>
     )
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/button";
+import { Button, Buttons } from "../components/button";
 import { H1 } from "../shared/ui-kit/h1";
 import { P } from "../shared/ui-kit/p";
 
@@ -6,10 +6,13 @@ export function MainPage() {
     return (
         <>
             <H1>사랑하는 사람에게 생일 카드를 작성해보세요! 🎈</H1>
-            <P>누구에게 생일 카드를 작성하실건가요?</P>
-            <Button>아버지 🧔</Button>
-            <Button>어머니 👸</Button>
-            <Button>형재 🧑👧</Button>
+            <P>누구에게 생일 카드를 작성하실건가요? ❤</P>
+        
+            <Buttons>
+                <Button>아빠 🧔</Button>
+                <Button>엄마 👸</Button>
+                <Button>누나 & 형 🧑👧</Button>
+            </Buttons>
         </>
     )
-}
+} 
